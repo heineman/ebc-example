1ffda437dccc6c058aaeff3847df91d2f9f57075
George Heineman <heineman@wpi.edu>
2020-08-20 23:27:47 -0400

    Initial GUI boundary object constructed using WindowBuilder

---
+++ EBC_SoftwareEngineeringProject/src/example/sliding/boundary/SlidingPuzzleApp.java
@@ -0,0 +1,105 @@
+package example.sliding.boundary;
+
+import java.awt.BorderLayout;
+import java.awt.EventQueue;
+
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+import javax.swing.border.EmptyBorder;
+import javax.swing.GroupLayout;
+import javax.swing.GroupLayout.Alignment;
+import javax.swing.JLabel;
+import javax.swing.JTextField;
+import javax.swing.LayoutStyle.ComponentPlacement;
+import javax.swing.JButton;
+
+public class SlidingPuzzleApp extends JFrame {
+
+	private JPanel contentPane;
+
+
+	/**
+	 * Create the frame.
+	 */
+	public SlidingPuzzleApp() {
+		setTitle("Sliding Puzzle Application");
+		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		setBounds(100, 100, 558, 464);
+		contentPane = new JPanel();
+		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
+		setContentPane(contentPane);
+		
+		JPanel panel = new JPanel();
+		
+		JLabel nmLabel = new JLabel("Number of Moves:");
+		
+		JLabel actualMoves = new JLabel("0");
+		
+		JButton btnReset = new JButton("Reset");
+		
+		JButton btnUp = new JButton("^");
+		
+		JButton btnDown = new JButton("v");
+		
+		JButton btnRight = new JButton(">");
+		
+		JButton btnNewButton = new JButton("<");
+		GroupLayout gl_contentPane = new GroupLayout(contentPane);
+		gl_contentPane.setHorizontalGroup(
+			gl_contentPane.createParallelGroup(Alignment.LEADING)
+				.addGroup(gl_contentPane.createSequentialGroup()
+					.addContainerGap()
+					.addComponent(panel, GroupLayout.PREFERRED_SIZE, 320, GroupLayout.PREFERRED_SIZE)
+					.addGroup(gl_contentPane.createParallelGroup(Alignment.LEADING)
+						.addGroup(gl_contentPane.createSequentialGroup()
+							.addGroup(gl_contentPane.createParallelGroup(Alignment.LEADING)
+								.addGroup(gl_contentPane.createSequentialGroup()
+									.addGap(18)
+									.addGroup(gl_contentPane.createParallelGroup(Alignment.LEADING)
+										.addGroup(gl_contentPane.createSequentialGroup()
+											.addComponent(nmLabel)
+											.addPreferredGap(ComponentPlacement.UNRELATED)
+											.addComponent(actualMoves, GroupLayout.PREFERRED_SIZE, 37, GroupLayout.PREFERRED_SIZE))
+										.addComponent(btnReset)))
+								.addGroup(gl_contentPane.createParallelGroup(Alignment.TRAILING)
+									.addGroup(gl_contentPane.createSequentialGroup()
+										.addPreferredGap(ComponentPlacement.RELATED)
+										.addComponent(btnDown))
+									.addGroup(Alignment.LEADING, gl_contentPane.createSequentialGroup()
+										.addGap(107)
+										.addComponent(btnUp))))
+							.addContainerGap(137, Short.MAX_VALUE))
+						.addGroup(Alignment.TRAILING, gl_contentPane.createSequentialGroup()
+							.addGap(62)
+							.addComponent(btnNewButton)
+							.addGap(50)
+							.addComponent(btnRight)
+							.addGap(119))))
+		);
+		gl_contentPane.setVerticalGroup(
+			gl_contentPane.createParallelGroup(Alignment.LEADING)
+				.addGroup(gl_contentPane.createSequentialGroup()
+					.addGroup(gl_contentPane.createParallelGroup(Alignment.LEADING, false)
+						.addGroup(gl_contentPane.createSequentialGroup()
+							.addContainerGap()
+							.addComponent(panel, GroupLayout.PREFERRED_SIZE, 400, GroupLayout.PREFERRED_SIZE))
+						.addGroup(gl_contentPane.createSequentialGroup()
+							.addGap(22)
+							.addGroup(gl_contentPane.createParallelGroup(Alignment.BASELINE)
+								.addComponent(nmLabel)
+								.addComponent(actualMoves))
+							.addGap(88)
+							.addComponent(btnUp)
+							.addPreferredGap(ComponentPlacement.RELATED)
+							.addGroup(gl_contentPane.createParallelGroup(Alignment.BASELINE)
+								.addComponent(btnRight)
+								.addComponent(btnNewButton))
+							.addGap(7)
+							.addComponent(btnDown)
+							.addPreferredGap(ComponentPlacement.RELATED, GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+							.addComponent(btnReset)))
+					.addContainerGap(GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE))
+		);
+		contentPane.setLayout(gl_contentPane);
+	}
+}
