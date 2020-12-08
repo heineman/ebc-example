f42a7a94784d61fd220be8ac2f3d4284bf78be55
George Heineman <heineman@wpi.edu>
2020-12-08 10:03:34 -0500

    bad character set

--- EBC_SoftwareEngineeringProject/src/oo/liskov/DoubleRightHandDetector.java
+++ EBC_SoftwareEngineeringProject/src/oo/liskov/DoubleRightHandDetector.java
@@ -1,16 +1,6 @@
 package oo.liskov;
 
-/**
- * Compute direction of the "elbow" (p1,p2) and (p2,p3).
- * 
- * | 1 p1x p1y |
- * | 1 p2x p2y | :   cp=(p2x–p1x)(p3y–p1y)–(p2y–p1y)(p3x–p1x)
- * | 1 p3x p3y |
- * 
- * If cp is negative, then the three points determine a right 
- * turn. If cp is positive then a left turn. If cp=0 then the
- * three points are colinear.
- */
+
 public class DoubleRightHandDetector implements IDetector {
 	
 	double p1x, p1y;
@@ -20,6 +10,17 @@ public class DoubleRightHandDetector implements IDetector {
 	// current one
 	int current = 1;
 	
+	/**
+	 * Compute direction of the "elbow" (p1,p2) and (p2,p3).
+	 * 
+	 * | 1 p1x p1y |
+	 * | 1 p2x p2y | :   cp=(p2x–p1x)(p3y–p1y)–(p2y–p1y)(p3x–p1x)
+	 * | 1 p3x p3y |
+	 * 
+	 * If cp is negative, then the three points determine a right 
+	 * turn. If cp is positive then a left turn. If cp=0 then the
+	 * three points are colinear.
+	 */
 	private double crossProduct() {
 		double cp =(p2x-p1x)*(p3y-p1y) - (p2y-p1y)*(p3x-p1x);
 		return cp;
